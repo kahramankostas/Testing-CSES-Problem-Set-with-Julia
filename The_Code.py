c27fb8d5c0786e879bf86250d6c8902d7ad8d167
Kahraman Kostas <36055021+kahramankostas@users.noreply.github.com>
2022-08-17 12:55:23 +0100

    Update The_Code.py

--- The_Code.py
+++ The_Code.py
@@ -1,1 +1,11 @@
-a=int(input())print(a,end = ' ')while a!=1:    if a%2==0:        a=a//2    else:        a=a*3+1    print(a, end = ' ')print()
+a=int(input())
+
+print(a,end = ' ')
+while a!=1:
+    if a%2==0:
+        a=a//2
+    else:
+        a=a*3+1
+    print(a, end = ' ')
+    
+print()
